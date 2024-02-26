a94620d658d06f6b7c3d6f75dac0c223b19ec51f
sajinrajan <sajinrajan082@gmail.com>
2024-02-26 12:44:14 +0530

    first class

---
+++ javascript.js
@@ -0,0 +1,183 @@
+// function color(col){
+//     document.getElementById("design").style.background=col
+//     }
+    
+    
+    
+    
+//                         function sachu(){
+    
+//                 var text =document.getElementById("demo").value
+//                 document.getElementById("imp").innerHTML=text
+//     if(text=="sajin"){
+//         alert(" you are great...")
+//     }
+    
+//                         }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+//             function show(){
+//                 sajin()
+//                 document.write("button clicked")
+//                 console.log("button clicked")
+               
+//                     var a=1
+//                     var b=2
+//                     var sum=a+b
+//                     document.write("sum="+sum)
+//                     console.log("sum="+sum)
+//     var c=4;
+//     var c=6
+//     console.log(c)
+//     document.write(6)
+    
+//     let e=5
+//     e=6
+//     console.log(e)
+    
+    
+//     const f=10
+    
+    
+    
+    
+    
+    
+//                     if(f%2==0){
+//                         console.log("number is even")
+    
+//                     }else{
+//                         console.log("number is odd")
+//                     }
+    
+    
+    
+    
+//             }
+    
+//             function sajin(){
+//     console.log("hii sajin ")
+//             }
+    
+    
+    //    array    
+
+
+//  1  
+ // let kitchenItem = getElementById("kitchenitem");
+    // let curryPowder =["salt", "pepper", "chilli","turmeric"];
+    // let li = document.querySelector("li");
+    // console.log(li)
+
+    //   2
+
+    // let curryPowder =["salt", "pepper", "chilli","turmeric"];
+    // let allLi =document.querySelectorAll("li")
+    // console.log(allLi)
+
+// 3
+
+//   let curryPowder =["salt", "pepper", "chilli","turmeric"];
+//   let allLi = document.querySelectorAll("li");
+//   allLi[0].innerText =curryPowder[0];
+//   allLi[1].innerText =curryPowder[1];
+//   console.log( allLi)
+
+// 4
+
+//  let kitchen =document.getElementById("kitchen");
+// let curryPowder =["salt", "pepper", "chilli","turmeric"];
+
+// for (let powder of curryPowder) {
+//     let li = document.createElement("li");
+//     li.innerText=powder;
+//     kitchen.appendChild(li)
+//     console.log(li);
+// }
+
+
+//   4
+
+  
+// let kitchenItemsList=document.getElementById("kitchen-items-list")
+// let curryPowder =["salt", "pepper", "chilli","turmeric"];
+// for(let powder of curryPowder){
+//     let li =document.createElement("li");
+//     li.innerText = powder
+//     kitchenItemsList.appendChild(li)
+//     console.log(li);
+// }
+
+// let kitchen =document.getElementById("kitchen");
+// const curryPowder =["salt", "pepper", "chilli","turmeric",];
+// for(let powder of curryPowder){
+//     let li = document.createElement("li");
+//     li.innerText = powder
+//     kitchen.appendChild(li)
+//     console.log(li)
+// }
+// const hi = ()=>{
+
+// }
+// const express =  require('express')
+// const app = express()
+
+// app.get('/hi',(req,res)=>{
+
+// })
+
+// let x = 10;
+// document.getElementById("demo").innerHTML = x;
+
+//STATEMENT
+
+// let P, Q, R;  
+// Q = 5;        
+// R = 6;        
+// P=Q+R  
+
+// document.getElementById("demo").innerHTML =
+// "The value of z is " + P + "."; 
+
+
+
+// let a, b, c;
+// a = 5;
+// b = 9;
+// c = a + b;
+// document.getElementById("statement").innerHTML = c;
+
+
+
+
+
+
+
+// FIND SUM OF 2 NUMBERS
+
+let a = 13;
+let b = 43;
+let c =a + b;
+console.log(c)
+
+// SHOW THE SUM IN WEB PAGE
+
+
+let x = 1;
+let y = 4;
+let z = x + y;
+
+document.getElementById('variable').innerHTML=(x+y)
+
